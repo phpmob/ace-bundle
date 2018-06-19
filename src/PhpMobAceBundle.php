e64c399f659c515be7f889acc0357f4e8ef451c7
liverbool <nukboon@gmail.com>
2018-06-19 18:03:03 +0700

    add extension name

--- src/PhpMobAceBundle.php
+++ src/PhpMobAceBundle.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace PhpMob\AceBundle;
 
+use PhpMob\AceBundle\DependencyInjection\PhpMobAceExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PhpMobAceBundle extends Bundle
 {
+    public function __construct()
+    {
+        $this->extension = new PhpMobAceExtension();
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBundlePrefix(): string
+    {
+        return $this->extension->getAlias();
+    }
 }
